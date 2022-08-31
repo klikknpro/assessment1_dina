1730b7d1338f17c1f89a7445825b120919685e65
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-08-31 11:19:06 +0200

    hundred-ten-millions work

--- number-to-words/src/utils/convertLogic.js
+++ number-to-words/src/utils/convertLogic.js
@@ -7,20 +7,36 @@ const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty'
 function convertLogic(num) {
   const input = num.toString();
 
+  const phrase = [];
+
   const digits = {
-    hundredMillion: input.length === 9 ? input[input.length - 9] : undefined,
-    tenMillion: input.length >= 8 ? input[input.length - 8] : undefined,
-    million: input.length >= 7 ? input[input.length - 7] : undefined,
-    hundredThousand: input.length >= 6 ? input[input.length - 6] : undefined,
-    tenThousand: input.length >= 5 ? input[input.length - 5] : undefined,
-    thousand: input.length >= 4 ? input[input.length - 4] : undefined,
-    hundred: input.length >= 3 ? input[input.length - 3] : undefined,
-    ten: input.length >= 2 ? input[input.length - 2] : undefined,
-    one: input.length >= 1 ? input[input.length - 1] : undefined,
+    hundredMillion: input.length === 9 ? parseInt(input[input.length - 9]) : undefined,
+    tenMillion: input.length >= 8 ? parseInt(input[input.length - 8]) : undefined,
+    million: input.length >= 7 ? parseInt(input[input.length - 7]) : undefined,
+    hundredThousand: input.length >= 6 ? parseInt(input[input.length - 6]) : undefined,
+    tenThousand: input.length >= 5 ? parseInt(input[input.length - 5]) : undefined,
+    thousand: input.length >= 4 ? parseInt(input[input.length - 4]) : undefined,
+    hundred: input.length >= 3 ? parseInt(input[input.length - 3]) : undefined,
+    ten: input.length >= 2 ? parseInt(input[input.length - 2]) : undefined,
+    one: input.length >= 1 ? parseInt(input[input.length - 1]) : undefined,
   };
 
+  if (digits.hundredMillion) phrase.push(ones[digits.hundredMillion], 'hundred');
+
+  if (digits.tenMillion === 1) phrase.push(teens[digits.million]);
+
+  if (digits.tenMillion >= 2) phrase.push(tens[digits.tenMillion - 2], '-');
+
+  if (digits.million >= 1 && digits.tenMillion >= 2) phrase.push(ones[digits.million]);
+  if (digits.tenMillion === 0 && digits.million >= 1) phrase.push(ones[digits.million]);
+  if (digits.tenMillion === undefined && digits.million >= 1) phrase.push(ones[digits.million]);
+
+  if (digits.million >= 0) phrase.push('million');
+
   console.log(digits);
-  return 'testing';
+  // ones[digits.hundredMillion] ((tens[digits.tenMillion] ones[digits.million]) || teens[digits.tenMillion]) hundred(if digits.hundredMillion true) million(if digits.million true)
+
+  return phrase.join(' ');
 }
 
 export default convertLogic;
@@ -35,4 +51,12 @@ input -> object (number)
 business logic test (jest)
 + testing-library
 !!! negativ, tort szamok
+! must not start with 0
+*/
+/*
+  const hundredMillionSuffix = digits.hundredMillion ? 'hundred' : '';
+  const millionSuffixMillion = digits.million ? 'million' : '';
+  const hundredThousandSuffix = digits.hundredThousand ? 'hundred' : '';
+  const thousandSuffix = digits.thousand ? 'thousand' : '';
+  const hundredSuffix = digits.hundred ? 'hundred' : '';
 */

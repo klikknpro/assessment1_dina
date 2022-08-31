1e342eec3292106825649ea9c91d563510487559
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-08-31 13:06:38 +0200

    working logic wo british version

--- number-to-words/src/utils/convertLogic.js
+++ number-to-words/src/utils/convertLogic.js
@@ -2,8 +2,6 @@ const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
 const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-// 980.565.125
-
 function convertLogic(num) {
   const input = num.toString();
 
@@ -21,11 +19,18 @@ function convertLogic(num) {
     one: input.length >= 1 ? parseInt(input[input.length - 1]) : undefined,
   };
 
-  if (digits.hundredMillion) phrase.push(ones[digits.hundredMillion], 'hundred');
+  // XXX.000.000
+  if (digits.hundredMillion) {
+    phrase.push(ones[digits.hundredMillion], 'hundred');
+    if (digits.tenMillion >= 1 || digits.million >= 1) phrase.push('and');
+  }
 
   if (digits.tenMillion === 1) phrase.push(teens[digits.million]);
 
-  if (digits.tenMillion >= 2) phrase.push(tens[digits.tenMillion - 2], '-');
+  if (digits.tenMillion >= 2) {
+    phrase.push(tens[digits.tenMillion - 2]);
+    if (digits.million >= 1) phrase.push('-');
+  }
 
   if (digits.million >= 1 && digits.tenMillion >= 2) phrase.push(ones[digits.million]);
   if (digits.tenMillion === 0 && digits.million >= 1) phrase.push(ones[digits.million]);
@@ -33,18 +38,52 @@ function convertLogic(num) {
 
   if (digits.million >= 0) phrase.push('million');
 
-  console.log(digits);
-  // ones[digits.hundredMillion] ((tens[digits.tenMillion] ones[digits.million]) || teens[digits.tenMillion]) hundred(if digits.hundredMillion true) million(if digits.million true)
+  // 000.XXX.000
+  if (digits.hundredThousand) {
+    phrase.push(ones[digits.hundredThousand], 'hundred');
+    if (digits.tenThousand >= 1 || digits.thousand >= 1) phrase.push('and');
+  }
 
-  return phrase.join(' ');
+  if (digits.tenThousand === 1) phrase.push(teens[digits.thousand]);
+
+  if (digits.tenThousand >= 2) {
+    phrase.push(tens[digits.tenThousand - 2]);
+    if (digits.thousand >= 1) phrase.push('-');
+  }
+
+  if (digits.thousand >= 1 && digits.tenThousand >= 2) phrase.push(ones[digits.thousand]);
+  if (digits.tenThousand === 0 && digits.thousand >= 1) phrase.push(ones[digits.thousand]);
+  if (digits.tenThousand === undefined && digits.thousand >= 1) phrase.push(ones[digits.thousand]);
+
+  if (digits.hundredThousand >= 1 || digits.tenThousand >= 1 || digits.thousand >= 1) phrase.push('thousand');
+
+  // 000.000.XXX
+  if (digits.hundred === 0) {
+    if (digits.ten >= 1 || digits.one >= 1) phrase.push('and');
+  }
+
+  if (digits.hundred) {
+    phrase.push(ones[digits.hundred], 'hundred');
+    if (digits.ten >= 1 || digits.one >= 1) phrase.push('and');
+  }
+
+  if (digits.ten === 1) phrase.push(teens[digits.one]);
+
+  if (digits.ten >= 2) {
+    phrase.push(tens[digits.ten - 2]);
+    if (digits.one >= 1) phrase.push('-');
+  }
+
+  if (digits.one >= 1 && digits.ten >= 2) phrase.push(ones[digits.one]);
+  if (digits.ten === 0 && digits.one >= 1) phrase.push(ones[digits.one]);
+  if (digits.ten === undefined && digits.one >= 1) phrase.push(ones[digits.one]);
+
+  const result = phrase.join(' ').replace(' - ', '-');
+
+  return result;
 }
 
 export default convertLogic;
-
-/*
-999.999.999
-nine hundred ninety-nine million * nine hundred ninety-nine thousand * nine hundred and ninety-nine
-*/
 
 /*
 input -> object (number)
@@ -52,11 +91,4 @@ business logic test (jest)
 + testing-library
 !!! negativ, tort szamok
 ! must not start with 0
-*/
-/*
-  const hundredMillionSuffix = digits.hundredMillion ? 'hundred' : '';
-  const millionSuffixMillion = digits.million ? 'million' : '';
-  const hundredThousandSuffix = digits.hundredThousand ? 'hundred' : '';
-  const thousandSuffix = digits.thousand ? 'thousand' : '';
-  const hundredSuffix = digits.hundred ? 'hundred' : '';
 */

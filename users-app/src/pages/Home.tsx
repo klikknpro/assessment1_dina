9fd0c5673ed533f9bda7da4acc17fa7da66771a4
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-09-09 20:05:52 +0200

    feat: basic pagination on home

--- users-app/src/pages/Home.tsx
+++ users-app/src/pages/Home.tsx
@@ -1,11 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 import { User } from '../utils/interfaces';
 import { getUsers } from '../api/diNaUsers';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import UserItem from '../components/UserItem';
 
 function Home() {
   const [userList, setUserList] = useState<User[]>([]);
+  const [indexOfFirst, setIndexOfFirst] = useState<number>(0);
+  const [tenUsers, setTenUsers] = useState<User[]>([]);
+
+  const handlePrevious = (): void => {
+    if (indexOfFirst >= 10) setIndexOfFirst((prevNum: number) => prevNum - 10);
+  };
+
+  const handleNext = (): void => {
+    if (indexOfFirst + 10 <= userList.length) setIndexOfFirst((prevNum: number) => prevNum + 10);
+  };
+
+  useEffect(() => {
+    const page = userList.slice(indexOfFirst, indexOfFirst + 10);
+    setTenUsers(page);
+  }, [indexOfFirst, userList]);
 
   useEffect(() => {
     let ignore = false;
@@ -24,11 +39,22 @@ function Home() {
   return (
     <Container>
       <Row xs={1} s={2} md={3} lg={4} className='g-3'>
-        {userList.map((user) => (
+        {tenUsers.map((user) => (
           <Col key={user.id}>
             <UserItem {...user} />
           </Col>
         ))}
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={handlePrevious}>Previous</Button>
+        </Col>
+        <Col>
+          {indexOfFirst} - {indexOfFirst + 10} of {userList.length}
+        </Col>
+        <Col>
+          <Button onClick={handleNext}>Next</Button>
+        </Col>
       </Row>
     </Container>
   );

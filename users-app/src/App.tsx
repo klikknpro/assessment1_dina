aa74fccc7802c7207f3a230f50f1901975fd6473
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-09-09 11:04:51 +0200

    feat(react-router): add navbar with react router

--- users-app/src/App.tsx
+++ users-app/src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NewUser from './pages/NewUser';
+import EditUser from './pages/EditUser';
+import NavMenu from './components/NavMenu';
 
 function App() {
   return (
     <div>
+      <NavMenu />
       <Container>
-        <h1>hey</h1>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<NewUser />} />
+          <Route path='/edit' element={<EditUser />} />
+        </Routes>
       </Container>
     </div>
   );
---
+++ users-app/src/components/NavMenu.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function NavMenu() {
+  return (
+    <div>
+      <Navbar expand='sm' className='bg-secondary shadow mb-4' variant='dark' sticky='top'>
+        <Container>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav>
+              <Nav.Link as={NavLink} to='/'>
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to='/new'>
+                New
+              </Nav.Link>
+              <Nav.Link as={NavLink} to='/edit'>
+                Edit
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavMenu;
